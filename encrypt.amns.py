d296a03f1ae0a3eb8be88c934e964c80ef07d25e
Jaden <99577888+JadenLau@users.noreply.github.com>
2025-01-07 15:55:41 +0000

    made new mode "key generation"

---
+++ encrypt.amns.py
@@ -0,0 +1,213 @@
+7Ab^ÿtJSt@c@g
+@@@@>((`xJrey8`}}} (~~oe~`G~{GG~oþor}>§
+@@@@>8Jþeþor{{þ~Gx8rx`xr}þ`yexe( x~ `>§
+@@@@>8ve(xovr  (}(xv~Gre}e}oG` y~oreG>§
+@@@@>~þo(þo ee{o{yrJ}~}y(e~eexJoJoex >
+d@*@\1@q13@6V$qTL@1EkTkq$&@V$4V@fq&L44@\$3$B$4L@k4@fZ\$3L\
+yA7<@+O(<]Ab{t@b<l7A+@+O(<@*@lA7^A SS@rJA
+b<l7A+@7S
+
+*@B$6RfZ@&16$3k1q
+
+{7pDx@c@>CpJr{( y^tb;K?<p7lOAS+wD|XWM$B6\L=TVk,R&mq1ZiE43f.s0÷zx~þ`Gv8}oe@2H:j*/-PFU%9'cÿug[d"CC	_5C>h§]QN#YnC+I!a>
+{7pD@c@g
+@@@@@@@@>Cpþ!64iq=}&(^C>~e93P[MnEvKrz-÷5*d"Nx§uHC+s {jp0OBTZ?ÿA	\tD17o,S_VW+%$G]lbCCL2y#gm`'h|:Y@/<wFU8JR;kf.QIXca>§
+@@@@@@@@>Cpþ?^!&ZG$ipy,QMEJVcnv4 O~Lx§C+B}A%[;b(eKz7F5/'+U.	_D\f:ogTrC>S"q1CCltuP<{sÿ3=-0w#÷h2*kHN6]W8Y|@`mjX9IdRa>§
+@@@@@@@@>CpþxYA,!E'Q27G^H.÷&§0nX<SB;Vse_qz:FoCCdp`C>K(+	-P1g?b$]U"y3mT}{/#5Rf D@W|k4ÿlir~uh9%[C+OjtMZw6*cL8va=\INJ>§
+@@@@@@@@>Cpþ0h.fQO!§-ub5/D?XTÿnJMCCNtL{	sr9&FS,6;+\mlv_$'=`eC+@iV~Wk}y^|pGEdKPU7zZ%23#(qag"4R<x*c AwjC>Y18][:o÷IHB>§
+@@@@@@@@>CpþUrVp9+!M=u<D}6i$GB5~^Jg3C>hC+?_[y*s(WdF'keP820Q.H X&1An§%t{-azcjN,o\KO|EÿZq4L;mvx:f]Sl#@CCbw"T`÷	7YI/R>§
+@@@@@@@@>Cpþ#MZWG§|cm3!Fwi"NLpkdz`(CCRn.}5~=^qefo$hA4DJ<b/uya E,[V÷8;'6_Q9Yv0O*lrUX\SB{@]KC+ÿ2C>&jx1H:+-P7%?tT	Igs>§
+@@@@@@@@>Cpþw8/}bDUQ~?÷P!nce.k\y`K§l3%zG*_,M5dC>0W]-uj XC+g2vaJrm94iÿoN7<H{	E&+$Oh'6LtVTq"F#^=1fA[;:x(RpSYBZsI|@CC>§
+@@@@@@@@>Cpþ`v	3A0<÷5?SrPn!;iZ7§+W@YjyVC+d.]RF}L=*tCC-(J,M NU%axOhf_mÿ8eC>ql6B^u:E2$g~pT#X{|4k'[HDKc"o&z1G\/9IbswQ>§
+@@@@@@@@>Cpþ:$k7{M#9'(uEf]n&!N %`*Fx-+|PTpQiC>§gR@bc6rdC+"[\eY13_aqB?2z,8CCLOWJU5t.ZwSlm^s	~GDh40/=KV÷HX<oAjyIv}ÿ;>§
+@@@@@@@@>CpþZs'}RB÷mH:\LGn6z!&e=l8O%o,1DC>W($h<."F+SdK9VbErY#_]Aa@^TMÿ-?	CC7igpj35U{4*/f[Xx;|u y§qJ20`tC+wPNQIkcv~>§
+@@@@@@@@>Cpþi:zS0|yqv$§~( D7n9b?!}"T=	P<sU8C+O\goxLJ,GCCw/ÿjF^f1p]Na{d%cV-.B@_l'+Z&m3C>h2Het[*uW5rY;E#XKAk4MQI6`R÷>§
+@@@@@@@@>Cpþ;b/÷%$q?(B9RjoWufnGPlQ!SZ*ÿ'g5UO L]CC8_p}N^,§	0\4|T3zYa+{x`K<r[H2XvC+EVhiJmAse#"FD-:c=6tk7C>yM@d&I1~w.>§
+@@@@@@@@>CpþWCC5Fic@,k[EQ}2|K1n-§HUD!&?*x9#t3sZ{yBGO6]v=N("TzXp:ÿmuaV4d+^L`	J/;8beho7SC+l<w C>$qrg'P.÷~ARYMfjI0%_\>§
+@@@@@@@@>Cpþ/[i 8_÷&dW	^MU,w:|un6N-c3O!4Gq5?TQ§'bLKCClek$@YÿSs%PVo*`aE{jZrH(yBX;C+pv0Df#RtAF\x~h}7+"g=1C>m<J]Iz2.9>§
+@@@@@@@@>CpþC+H{P/e8\|0$Y`[<pndGBRCCC>rc!2vVFAW÷,QDgKz#:^TUsXkh-u§i]"a@SL9=	(*1lO3w4?o+Eÿ}Mt.y&jq6J_'7 xbN5m;I%Z~f>§
+@@@@@@@@>CpþfJ~xyMC>Fu2/AE"*`Bn =NHZ§TWb!@?DPgVUX{$_rz[^<5w1-s#0÷Y9ÿpOa;R.Q+S',etj|hCC]6C+\m}Lkd734lo:K	(Gi&vI%8qc>§
+@@@@@@@@>CpþRCCtebwoT4p_<."xPg~nA9=MiOC>zN!FJGDsV	l$X5+C+7[*\|023Uh1§^Ea@ }u?Y(q`fj'#÷:{&8rQH%]ÿmd,;/cBZKykLSIW6v->§
+@@@@@@@@>Cpþ\ueK92_M1Tm ^j(c{yEdnShq*$YZ`bCCQ!A,?OFC>Ls"5'	=}8C+.6:z]~÷-a7§@R4wx[N%PtkfJDBl/Hv0|WXrUV#+gopG<&I;3ÿi>§
+@@@@@@@@>Cpþ(lXH_fOF9riU^$.;DcQd'g\CC+WEnMY}B1ÿC>#T![/&,e	~p"0mJ=Nq÷G?§]P*sah<LK|Zu`53C+%86y@tAwv-bVj4Ro2:{ SIxkz7>§
+@@@@@@@@>Cpþ-jhs;3Gq9dEt~mvA]xy#*5gl÷bLoVzÿ|\	[$u8NB`!nTW6r?Zf=UK"i2e}^{p7.+aS1C>&§Q:w'JHPC+(XFcRk/<,MO_0D@% CC4YI>§
+@@@@@@@@>CpþBT9\N7i1k+'÷}M(§Y_5C>	3l ".$f:b8@]|/sh4^Lrn!O-U*0jEeXWAo2Sut`GJa=K{PRC+H[~v;xcz#CC%<dqF,pÿ?6&VZDQgwmyI>§
+@@@@@@@@>Cpþ5]C>0t@y}J#KWDXmUR_SckYg{Oz9QjNls2~%ex6$'o7nr!"÷bq;:C+-|P\=HAv,/§fau.MB3dE pGTh*^14[?iZ+F<ÿw(VCC`8&L	I>§
+@@@@@@@@>CpþP9eQM*18C>fqR	|x,7VFk2ZX6~{WG/jOoL-'÷Y& E<n+s!?"=:^tigS0}T[%5m_v]a`cÿ#$uDzIh4;3dw§pJ.y\blC+ACCNrBH(KU@>§
+@@@@@@@@>Cpþ7`Wt#f+.0uY"U%:8<*kbAOG\d,j&CCh4;~RyKr5QNM9mcn@B_!]2[-Ez}pg?÷=§|J6aHD$sV(/xI 	1'ÿLS^oZwleC+qC>T{3FPXiv>
+d
+
+( y@+<lÿ{? JA%95
+@@@@lAbp+%>Ceþþge<E <7Dbp^@J??@ p{AWl+b7p@+ <l@yb? S]]]>9
+@@@@7S]SWS+ <%>Sw(7@A<@'yD@Q+<lQ p{AWl+b7pU]+<lCpSw(7@A<@'yD@HQ p{AWl+b7pU]+<lCp*@F@( ?@Qy@-+ <l-CC p{AWl+b7pU]+<l>9
+@@@@lAbp+%>4w{{ SSyw?]Cp>9
+lAbp+%>Ceþþgx<Z? JS @{t {K@+t @tJSt@r ?7|@+7@K  l@W7wA@(J+J@SJy ]Ceþþge<CpCeþþgov<'''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''>9
+
+*@ ly@c@gd
+ ly@c@7l p%S+A%ÿÿyb? ÿÿ9§>A>9]A J(%9
+
+hhh
+y7A@b@bp@7l p%S+A%ÿÿyb? ÿÿ9§>A>9]A J(%95
+@@@@ ly@uc@gS+A%b9]S+Abl%>Cp>9d
+hhh
+
+ (Jt§@ (rt§@ lyJ§@ lyr@c@%
+@@@@ÿÿb<l7A+ÿÿ%>tJSt?br>9]<(G%S+A%{7pDx9] p{7( %99]t X(b^ S+%9§
+@@@@ÿÿb<l7A+ÿÿ%>tJSt?br>9]<(G%S+A%{7pD9] p{7( %99]t X(b^ S+%9§
+@@@@ÿÿb<l7A+ÿÿ%>tJSt?br>9]<(G%S+A% lyge5o~d9] p{7( %99]t X(b^ S+%9§@*@ lyJ@Jp(@ lyr@{J?{w?J+ (@<JpwJ??W
+@@@@ÿÿb<l7A+ÿÿ%>tJSt?br>9]<(G%S+A% lygx8G5? p% ly9d9] p{7( %99]t X(b^ S+%9
+9
+
+S+J+wS@c@>Ceþþgo~<Z$44L\>@by@7Ab^ÿtJStged@cc@ (Jt@ ?S @>Ceþþgox<=$k&L\>
+lAbp+%y>Ceþþgov<Lp{AWl+b7p@\J+JrJS @$@VJSt5@[ÿÿb<l7A+ÿÿ%>tJSt?br>9]<(G%S+A%{7pDx9] p{7( %99]t X(b^ S+%9"@[S+J+wS">9
+S+J+wS@c@>Ceþþgo~<Z$44L\>@by@7Ab^ÿtJStgxd@cc@ (rt@ ?S @>Ceþþgox<=$k&L\>
+lAbp+%y>Ceþþgov<Lp{AWl+b7p@\J+JrJS @B@VJSt5@[ÿÿb<l7A+ÿÿ%>tJSt?br>9]<(G%S+A%{7pD9] p{7( %99]t X(b^ S+%9"@[S+J+wS">9
+S+J+wS@c@>Ceþþgo~<Z$44L\>@by@7Ab^ÿtJStg~d@cc@ lyJ@ ?S @>Ceþþgox<=$k&L\>
+lAbp+%y>Ceþþgov<Lp{AWl+7A@ZJA+@$@VJSt5@@@@@@[ lyJ"@[S+J+wS">9
+S+J+wS@c@>Ceþþgo~<Z$44L\>@by@7Ab^ÿtJStgþd@cc@ lyr@ ?S @>Ceþþgox<=$k&L\>
+lAbp+%y>Ceþþgov<Lp{AWl+7A@ZJA+@B@VJSt5@@@@@@[ lyr"@[S+J+wS">9
+
+
+lAbp+%>Ceþþgov<'''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''>9
+lAbp+%
+@@@@>Ceþþgov<VJSt@4+J+wS5@Ceþþgo~<1R>@@
+@@@@by@7Ab^ÿtJStged@cc@ (Jt@Jp(@7Ab^ÿtJStgxd@cc@ (rt@Jp(@7Ab^ÿtJStg~d@cc@ lyJ@Jp(@7Ab^ÿtJStgþd@cc@ lyr@ ?S 
+@@@@>Ceþþgov<VJSt@4+J+wS5@Ceþþgox<=$k&>
+9
+lAbp+%>Ceþþgov<'''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''Ceþþge<Cp>9
+
+by@7Ab^ÿtJStged@:c@ (Jt@7A@7Ab^ÿtJStgxd@:c@ (rt5
+@@@@b@c@bplw+%>Ceþþgoþ<g::d@\J+JrJS @(7 S@p7+@<J+{t@+t @yJ{+7AW@tJSt]Cp$S@J@A Sw?+§@W7w@|b??@^ +@{7AAwl+ (@yb? S]Cp@67p+bpw Y@gW§pd5@Ceþþge<>9
+@@@@by@b@:c@>W>5
+@@@@@@@@lAbp+%>LXb+bp^]>9_@ Xb+%~9
+
+by@7Ab^ÿtJStg~d@:c@ lyJ@7A@7Ab^ÿtJStgþd@:c@ lyr5
+@@@@b@c@bplw+%>Ceþþgoþ<g::d@Lp{AWl+7A@(7 S@p7+@<J+{t@b+S@yJ{+7AW@tJSt]Cp$S@J@A Sw?+§@W7wA@ p{AWl+ (@yb? S@<JW@r @wpSJy ]Cp@67p+bpw Y@gW§pd5@Ceþþge<>9
+@@@@by@b@:c@>W>5
+@@@@@@@@lAbp+%>LXb+bp^]>9_@ Xb+%þ9
+
+by@p7+@%@>' >@bp@ÿÿb<l7A+ÿÿ%>SWS>9]JA^D@7A@>'(>@bp@ÿÿb<l7A+ÿÿ%>SWS>9]JA^D@7A@>'A>@bp@ÿÿb<l7A+ÿÿ%>SWS>9]JA^D@95
+@@@@+<lÿ{? JA%9
+@@@@b@c@bplw+%> @+7@ p{7( §@(@+7@( {7( @7A@A@+7@Awp@ p{7( (@lA7^AJ<@g §(§Ad5@>9
+ ?S 5@b@c@?bS+%S+A%ÿÿb<l7A+ÿÿ%>SWS>9]JA^Dgxd99gxd
+by@b@cc@> >@7A@>' >@bp@ÿÿb<l7A+ÿÿ%>SWS>9]JA^D5
+@@@@by@p7+@>' >@bp@ÿÿb<l7A+ÿÿ%>SWS>9]JA^D5
+@@@@@@@@b@c@bplw+%>yb? 5@>9
+@@@@@@@@yb@c@7l p%yb? cb§<7( c>A>§ p{7(bp^c>w+y'}>9
+@@@@@@@@7@c@bplw+%>7w+lw+@yb? @%{A J+ @p |§@U]J<pS@A {7<< p( (95@>9
+@@@@@@@@y7@c@7l p%yb? c7§<7( c>|>§ p{7(bp^c>w+y'}>9
+@@@@ ?S 5
+@@@@@@@@b@c@ÿÿb<l7A+ÿÿ%>SWS>9]JA^Dg~d
+@@@@@@@@7@c@ÿÿb<l7A+ÿÿ%>SWS>9]JA^Dgþd
+@@@@@@@@yb@c@7l p%yb? cb§<7( c>A>§ p{7(bp^c>w+y'}>9
+@@@@@@@@y7@c@7l p%yb? c7§<7( c>|>§ p{7(bp^c>w+y'}>9
+@@@@yb@c@yb]A J(%9
+@@@@?JS+§@A Sw?+@c@S+A%yb9§S+A%yb9
+@@@@{7pD~@c@["
+@@@@rJA@c@+O(<%+7+J?c? p%{7pD9U? p%S+A%%?JS+9999
+@@@@y7A@J@bp@AJp^ %? p%{7pD995@*@{7pDg~þd@<JXb<w<
+@@@@@@@@rJA]S +ÿ( S{Abl+b7p%y>[J"@7y@[? p%{7pD9">9
+@@@@@@@@{7pD~]{? JA%9
+@@@@@@@@{7pD~@c@["
+@@@@@@@@y7A@b@bp@AJp^ %? p%{7pDx995@*@{7pDxgbd@'#@{7pDgJdgbd@{A J+ @{7pD A+ A
+@@@@@@@@@@@@{7pD~g{7pDxgbdd@c@?bS+%{7pDgJd9gbd
+@@@@@@@@lA A Sw?+@c@hh
+@@@@@@@@y7A@r@bp@AJp^ %? p%S+A%?JS+9995@*@{7pD A+@7Ab^bpJ?@+7@ p{AWl+ (
+@@@@@@@@@@@@lA A Sw?+@uc@{7pD~g?bS+%S+A%?JS+99grdd
+@@@@@@@@@@@@rJA]wl(J+ %x9
+@@@@@@@@?JS+@c@lA A Sw?+
+@@@@@@@@A Sw?+@c@S+A%lA A Sw?+9
+@@@@@@@@lAbp+%y>(J+J@? p^+t@|Ab++ p5@[? p%lA A Sw?+9">9
+@@@@y7]|Ab+ %A Sw?+9
+@@@@rJA]{?7S %9
+@@@@lAbp+%>(7p ]>9
+ ?by@b@cc@>(>@7A@>'(>@bp@ÿÿb<l7A+ÿÿ%>SWS>9]JA^D5
+@@@@by@p7+@>'(>@bp@ÿÿb<l7A+ÿÿ%>SWS>9]JA^D5
+@@@@@@@@b@c@bplw+%>yb? 5@>9
+@@@@@@@@yb@c@7l p%yb? cb§<7( c>A>§ p{7(bp^c>w+y'}>9
+@@@@@@@@7@c@bplw+%>7w+lw+@yb? @%{A J+ @p |95@>9
+@@@@@@@@y7@c@7l p%yb? c7§<7( c>|>§ p{7(bp^c>w+y'}>9
+@@@@ ?S 5
+@@@@@@@@b@c@ÿÿb<l7A+ÿÿ%>SWS>9]JA^Dg~d
+@@@@@@@@yb@c@7l p%yb? cb§<7( c>A>§ p{7(bp^c>w+y'}>9
+@@@@@@@@7@c@ÿÿb<l7A+ÿÿ%>SWS>9]JA^Dgþd
+@@@@@@@@y7@c@7l p%yb? c7§<7( c>|>§ p{7(bp^c>w+y'}>9
+@@@@{7pD]A D AS %9
+@@@@yb@c@yb]A J(%9
+@@@@?JS+§@A Sw?+@c@S+A%yb9§S+A%yb9
+@@@@{7pD~@c@["
+@@@@rJA@c@+O(<%+7+J?c? p%{7pD9U? p%S+A%%?JS+9999
+@@@@y7A@J@bp@AJp^ %? p%{7pD995@*@{7pDg~þd@<JXb<w<
+@@@@@@@@rJA]S +ÿ( S{Abl+b7p%y>[J"@7y@[? p%{7pD9">9
+@@@@@@@@{7pD~]{? JA%9
+@@@@@@@@{7pD~@c@["
+@@@@@@@@y7A@b@bp@AJp^ %? p%{7pDx995@*@{7pDxgbd@'#@{7pDgJdgbd@{A J+ @{7pD A+ A
+@@@@@@@@@@@@{7pD~g?bS+%{7pDgJd9gbdd@c@{7pDxgbd
+@@@@@@@@lA A Sw?+@c@hh
+@@@@@@@@y7A@r@bp@AJp^ %? p%S+A%?JS+9995
+@@@@@@@@@@@@lA A Sw?+@uc@{7pD~g?bS+%S+A%?JS+99grdd
+@@@@@@@@@@@@rJA]wl(J+ %x9
+@@@@@@@@?JS+@c@lA A Sw?+
+@@@@@@@@A Sw?+@c@S+A%lA A Sw?+9
+@@@@@@@@lAbp+%y>[J"@7y@[? p%{7pD9"@{7<l? + (§@(J+J@? p^+t@[? p%lA A Sw?+9">9
+@@@@y7]|Ab+ %A Sw?+9
+@@@@rJA]{?7S %9
+@@@@lAbp+%>(7p ]>9
+ ?by@b@cc@>A>@7A@>'A>@bp@ÿÿb<l7A+ÿÿ%>SWS>9]JA^D5
+@@@@by@p7+@>'A>@bp@ÿÿb<l7A+ÿÿ%>SWS>9]JA^D5@b@c@bplw+%>yb? 5@>9
+@@@@ ?S 5@b@c@ÿÿb<l7A+ÿÿ%>SWS>9]JA^Dg~d
+@@@@yb@c@7l p%yb? cb§<7( c>A>§ p{7(bp^c>w+y'}>9
+@@@@7@c@yh p{AWl+b7p[ÿÿb<l7A+ÿÿ%>AJp(7<>9]AJp(bp+%xx`Gx`§eXxoxo}xe9"]+<lh
+@@@@y7@c@7l p%yb? c7§<7( c>|>§ p{7(bp^c>w+y'}>9
+@@@@{7pD]A D AS %9
+@@@@yb@c@yb]A J(%9
+@@@@?JS+§@A Sw?+@c@S+A%yb9§S+A%yb9
+@@@@{7pD~@c@["
+@@@@rJA@c@+O(<%+7+J?c? p%{7pD9U? p%S+A%%?JS+9999
+@@@@yJb?S@c@e
+@@@@yrW+ S@c@e
+@@@@y7A@J@bp@AJp^ %? p%{7pD995@*@{7pDg~þd@<JXb<w<
+@@@@@@@@rJA]S +ÿ( S{Abl+b7p%y>[J"@7y@[? p%{7pD9"§@[yJb?S"@{7AAwl+ (@r?7{KS§@[yrW+ S"@{7AAwl+ (@rW+ S>9
+@@@@@@@@{7pD~]{? JA%9
+@@@@@@@@{7pD~@c@["
+@@@@@@@@y7A@b@bp@AJp^ %? p%{7pDx995@*@{7pDxgbd@'#@{7pDgJdgbd@{A J+ @{7pD A+ A
+@@@@@@@@@@@@{7pD~g?bS+%{7pDgJd9gbdd@c@{7pDxgbd
+@@@@@@@@lA A Sw?+@c@hh
+@@@@@@@@bSyJb?@c@=J?S 
+@@@@@@@@y7A@r@bp@AJp^ %? p%S+A%?JS+9995
+@@@@@@@@@@@@+AW5
+@@@@@@@@@@@@@@@@lA A Sw?+@uc@{7pD~g?bS+%S+A%?JS+99grdd
+@@@@@@@@@@@@@@@@rJA]wl(J+ %x9
+@@@@@@@@@@@@ X{ l+5
+@@@@@@@@@@@@@@@@yJb?S@uc@x
+@@@@@@@@@@@@@@@@bSyJb?@c@3Aw 
+@@@@@@@@?JS+@c@lA A Sw?+
+@@@@@@@@A Sw?+@c@S+A%lA A Sw?+9
+@@@@@@@@by@bSyJb?5@yrW+ S@uc@? p%lA A Sw?+9
+@@@@@@@@lAbp+%y>[J"@7y@[? p%{7pD9"@{7<l? + (§@(J+J@? p^+t@[? p%lA A Sw?+9"§@+7+J?@[yJb?S"@{7AAwl+ (@r?7{KS§@+7+J?@[yrW+ S"@{7AAwl+ (@rW+ S>9
+@@@@y7]|Ab+ %A Sw?+9
+@@@@rJA]{?7S %9
+@@@@lAbp+%>{7<lb? (]>9
+@@@@by@yJb?S@:c@e5@lAbp+%y>Ceþþgoþ<g::d@[yrW+ S"@rW+ S@bp@[yJb?S"@r?7{KS@tJS@r  p@y7wp(@JS@{7AAwl+ (:Ceþþge<>9
+@@@@by@p7+@%@>' >@bp@ÿÿb<l7A+ÿÿ%>SWS>9]JA^D@7A@>'(>@bp@ÿÿb<l7A+ÿÿ%>SWS>9]JA^D@7A@>'A>@bp@ÿÿb<l7A+ÿÿ%>SWS>9]JA^D@95
+@@@@@@@@?Jp^@c@q7p 
+@@@@@@@@?Jp^@c@bplw+%> p+ A@{7<lb? A@lJ+t@7A@{7<<Jp(@% XJ<l? 5@lW§@blW+t7pþ§@;JDJ95@>9
+@@@@@@@@by@?Jp^@cc@q7p @7A@?Jp^@cc@>>5@?Jp^@c@>lW+t7p>
+@@@@@@@@lJAJx@c@bplw+%> p+ A@lJAJ< + A@r y7A @yb? pJ< ]@by@p7§@+Wl @p7+tbp^@Jp(@{7p+bpw 5@>9
+@@@@@@@@lJAJ~@c@bplw+%> p+ A@lJAJ< + A@Jy+ A@yb? pJ< ]@by@p7§@+Wl @p7+tbp^@Jp(@{7p+bpw 5@>9
+@@@@ ?S 5
+@@@@@@@@+AW5@?Jp^@c@ÿÿb<l7A+ÿÿ%>SWS>9]JA^Dgþd
+@@@@@@@@ X{ l+5@?Jp^@c@>lW+t7p>
+@@@@@@@@+AW5@lJAJx@c@ÿÿb<l7A+ÿÿ%>SWS>9]JA^Dg`d
+@@@@@@@@ X{ l+5@lJAJx@c@>>@
+@@@@@@@@+AW5@lJAJ~@c@ÿÿb<l7A+ÿÿ%>SWS>9]JA^DgGd
+@@@@@@@@ X{ l+5@lJAJ~@c@>>
+@@@@@@@@by@lJAJx@cc@>p7p >@7A@lJAJx@cc@>q7p >5@lJAJx@c@>>
+@@@@@@@@by@lJAJ~@cc@>p7p >@7A@lJAJ~@cc@>q7p >5@lJAJ~@c@>>@
+@@@@*@lAbp+%y>lA7^AJ<@?Jwp{t@{7<<Jp(5@[?Jp^"@[lJAJx"@[7"@[lJAJ~">9
+@@@@y7]{?7S %9
+@@@@7S]SWS+ <%y><D@[7"@Q+<lQ[7"@Cp*@F@<7D @[7"@-+ <l-CC[7">9
+@@@@7S]SWS+ <%y>[?Jp^"@[lJAJx"@Q+<lQ[7"@[lJAJ~"@*@F@S+JA+@|+@[?Jp^"@[lJAJx"@-+ <l-CC[7"@[lJAJ~">9
+@@@@
+*@6+A?uRuf@+7@A <7D @p7+ @
+*@6+A?uRu6@+7@J((@p7+ 
