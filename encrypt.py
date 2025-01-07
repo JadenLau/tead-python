27f67a051415aae6058ac813547a4a51120ea926
Jaden <99577888+JadenLau@users.noreply.github.com>
2025-01-07 08:06:14 +0000

    make some update

--- encrypt.py
+++ encrypt.py
@@ -1,41 +1,41 @@
 from tqdm.rich import tqdm # progress bar
 import os
 
-conv1 = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890 `~!@#$%^&*()-=_+[{]}\\|;:\'",./<>?§\n\tÿþ÷'
+conv1 = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890 `~!@#$%^&*()-=_+[{]}\\|;:\'",./<>?§\n\tÿþ÷¥€£…'
 conv = [
-        '3þCSQNF8Ldg\'20)T^{z§R6kbZ%Y:#]}<1,+~\tWec@nX\nqBGPl_r|DhvOo9Js;Hyt(A5.pi\\E`f>[M4-"w!? $mu&*7aKjIUV/ÿx=÷',
-        '3lgþLP5AQnfJ/zRaH=§6Seq2E1,\tB8r({jid\n0kZo&:$-t*V|;vDU!9[Gb\'s}NO\\ph+^mcW_TF%Xu>Y"`#I~<C.y7?w 4M@x)ÿ]K÷',
-        '31?rJþR-/`o5g~VYL,X§xmsBjHW0;NZ!&9\\]n4\'kdt|%^O[liA.*}fTMG8c$>:KUev ywIS_pQb2+")({\n\tq@hzPuC#=E76÷FDÿ<a',
-        '3X"VU/qþ,%+i:\n$vlxG_§az\\<hEc|Wb)L&sJCjtDMp6;A-F40\t QH2yI8fgwn5R]k^*oZP(`T>dN÷[}SKm1#=eru@\'?O7.{!9Yÿ~B',
-        '3\n*bHn)tþzF+mv8CQA5B:2ga[T\'"\tl;{f#Wdy]&-I0^7`X/V~exLOr§,(hc%÷Z=@<J9DkqwR_PNSEjM61!U.sp> \\iu}G4Y|o?ÿ$K',
-        '3>zPy5,w=MTþ&uQ}<EnI]Z4d\\K§V8:2FgN0U\n9A"rSvami$+f÷eRJ{HY7j-C;/)?6Xq#pb*xDsBc .k\t_`\'L@1O~!t%^o(lhG|ÿ[W',
-        '3u7$8iv*/2lY^þ§=0VIDf4k,pT(Z5#;Jz:]\'Xy.%+@ex\n\t[`6÷abM)SQ_9<om~c|RLtAq"-CEhHGN}&>gFOUr{j!1dKns?BPWÿw \\',
-        '346|TrXmY:lsb^§þjQP\no,ty ?@fH\t]V.K&8EF#h\\%daJze<*(÷1q"U;M_70\'NpCBg+!R`A[2nG>xcwSI-{~vk=}9LZO5D$)ÿiWu/',
-        '3!AIocz>)-d+RU.§Lþ<e(4#&1%tw^Gn/Q\',[K i=Cb]\t}{D0?OT;÷NBl`ZJ7\\Eqya*:hVPuspMgW|25v"SX$FkHY~\nxm9r@fÿ68_j',
-        '3P\nW-8KBYM~!DE5§CZþL0Fp7q(9JOv\'ydA"mV}&ts]k)HiRb?>;.r÷ gGz_%l|\\oQ[n@T:*cS#$U{x1jw+ef,Na`X4h\tu^</ÿI=62',
-        '3Q!ZsXwfN6A,2devo§)ilþ8}GF|^mW*7\tqD[91EaJ5\\u$_@&gUOn.<÷c](=H%VB ;p-tPLMT\'"`\n~0h{#+y:b?jR>xkrISz/ÿC4KY',
-        '3ji$Y(ANldB)K@9y+U§5^p/þsP#_-[:*qeE.\\7;n8<gJ,|\nXDSwGTZ?÷tc14kmb{~`x6\tRH"QaMrW0>}&v%!=FChIo\'fz ]LÿO2uV',
-        '3y\\:&Q= JI{R/8`wkO§%,~*vþLl#1)>hTWPcfB5qC.6F<d}GZxn\n!_M+÷HS]tgE4|a$j7i0"9os\tpmue\'ANb[-^VY2rK?zU@ÿX(;D',
-        '3${Qe7;YL]y|gz*Ju!w+§C<%=TqþS5N:lG\n/,-iEk\\p0IA ?_sW(^H9#4÷Rc@Pb~dfBxj\tn6XvU>Khr&D12"8ot}[FO\'Mma.ÿZ`V)',
-        '3\t~c^$07DwXA?4{mn§]5BK\\\'b=þ`6H&ryYJ/v[kZ>!gG*WxI"%+,Q.\n}÷ sE)F|d#OpqTuSl9tR_8zhVfL@NCa;-oe1i<:Mjÿ(P2U',
-        '3Ua21fz\'&+`$rR}#4B§eF<~P,\nGyiþ lv^[H*xcA;bZ{gm:uO%W>XY?)_nq÷jKV/ts-J0h@w"\\.C\tDM8EI]oTSp9!k|d5QL6ÿ(7N=',
-        '3K\\h0iu9GSn;mV}1^[2§r)\nFzQq\'Z<þ&a5vWH|pAx:t\to{#DwX`T*"O,gR÷ e8+l?dN4U@->Y!cL7b/~(._M]Jj$=BPkfIEsÿyC6%',
-        '3D+0k)`;zOGMeg@d=cfR]§s"N#A?P\n4i\\/þrJlq&\'EW}:-|F87\tVC!Z.2Y%÷o, KSu1{<(^hIUavBp$~6Xwyxb*H>t[9n5mLÿjT_Q',
-        '3dpx~;Uq&)bQ*gAVjv=/]-[D\\tyR§z?8BO_\'>Gþ{$LJ0|2n}XMaF<NY5l,.^#W÷"mEkwP+4:T\t(7Cf hru6%\niH@SK9`!cesÿ1IZo',
-        '3%@"WjT5N)]Rh2M6r.1f>#:[pYiE9HZ_wD|{A+7<B4þ§GyCblPUF*k}Q`08gcnoVt÷sO\'L,/!u-a~^\tdx&=KI$mJzq;Xv (\ne\\S?ÿ',
-        '3BG)D<oQOIt-Y8zd,?;:\'|Tpe}VAU!i7 .w$W"SgEb§þq%*#X@R0xyr9`s\n+h45a÷Fkc^K\t~{26j1=Z>\\(m]N&Jn_lCLHPv/[uMfÿ',
-        '3:.\'Xh f8a>kyvxM*K;s=I?[cqZ)/@<pW`2(01CA-9o§bþ}YiNj!\t%w^DF~r6J$,U÷+VzBT]Ren5G\n"#gOS{lQPt&m_udH\\47LE|ÿ',
-        '3^)0/z#O7\'UNK|w1JoH&I`PxC2\ncy5$@q9E%-Y?LeRm§tWþl}F!ghQ[sX8G{(:M;6.÷4=_>A+vZÿ"SjT]u,naVfDip\tr\\<bB~dk* ',
-        '3o4yh>UtVX+?}*(!7m#Iirq5D]J@L\\"Sj2Kfkb:/<z)M=§ B;þ.`{%RZ8n[lY\nF,waC÷~vAWHd$1ÿe|O-_Esg9Pup0\tN\'GcT&^xQ6'
+        '3þCSQNF8Ldg\'20)T^{z§R6kbZ%Y:#]}<1,+~\tWec@nX\nqBGPl_…r|DhvOo9Js;Hyt(A5.pi\\E`f>[M4-"w!? $mu&¥€£*7aKjIUV/ÿx=÷',
+        '3lgþLP5AQnfJ/zRaH=§6Seq2E1,\tB8r({jid\n0kZo&:$-t*V|;vDU!9[Gb\'s}NO\\ph+^mcW_TF%Xu>Y"`#I~<C.y7?w 4M@x¥€£…)ÿ]K÷',
+        '31?rJþR-/`o5g~VYL,X§xmsBjHW0;NZ!&9\\]n4\'kdt|%^O[liA.*}fTMG8c$>:KUev ywIS_pQb2+")({\n\tq@hzPuC#=¥€…£E76÷FDÿ<a',
+        '3X"VU/qþ,%+i:\n$vlxG_§az\\<hEc|Wb)L&sJCjtDMp6;A-F40\t QH2yI8fgwn5R]k^*oZP(`T>dN÷[}S¥€£Km1#=eru@\'?O7.{!9…Yÿ~B',
+        '3\n*bHn)tþzF+mv8CQA5B:2ga[T\'"\tl;{f#Wdy]&-I0^7`X/V~exLOr§,(hc%÷Z=@<J9DkqwR_PNSEjM61!U.sp> ¥€£\\iu}G4Y|o?ÿ…$K',
+        '3>zPy5,w=MTþ&uQ}<EnI]Z4d\\K§V8:2FgN0U\n9A"rSvami$+f÷eRJ{HY7j-C;/)?6Xq#pb*xDsBc .k\t_`\'L@1O~!t%^o(¥€£…lhG|ÿ[W',
+        '3u7$8iv*/2lY^þ§=0VIDf4k,pT(Z5#;Jz:]\'Xy.%+@ex\n\t[`6÷abM)SQ_9<om~c|RLtAq"-CEhHGN}&>gFOUr{j!1d…Kn¥€£s?BPWÿw \\',
+        '346|TrXmY:lsb^§þjQP\no,ty ?@fH\t]V.K&8EF#h\\%daJze<*(÷1q"U;M_70\'NpCBg+!R`A[2nG>xcwSI-{~vk=}…9LZO5D$¥€£)ÿiWu/',
+        '3!AIocz>)-d+RU.§Lþ<e(4#&1%tw^Gn/Q\',[K i=Cb]\t}{D0?OT;÷NBl`ZJ7\\Eqya*:hVPuspMgW|25v"SX$FkH…Y~\nxm9r@fÿ68_j¥€£',
+        '3P\nW-8KBYM~!DE5§CZþL0Fp7q(9JOv\'ydA"mV}&ts]k)HiRb?>;.r÷ gGz_%l|\\oQ[n@T:*cS#$U{x1jw+ef,…Na`X4h\tu^</ÿI¥€£=62',
+        '3Q!ZsXwfN6A,2devo§)ilþ8}GF|^mW*7\tqD[91EaJ5\\u$_@&¥€£gUOn.<÷c](=H%VB ;p-tPLMT\'"`\n~0h{#+y:b?jR>xkrISz/ÿC4K…Y',
+        '3ji$Y(ANldB)K@9y+U§5^p/þsP#_-[:*qeE.\\7;n8<gJ,|\nXDSwGTZ?÷tc¥€£14kmb{~`x6\tRH"QaMrW0>}&v%!=FChIo\'fz ]LÿO2u…V',
+        '3y\\:&Q= JI{R/8`wkO§%,~*vþLl#1)>hTWPcfB5qC.6F<d}GZxn\n!_M+÷HS]tgE4¥€£|a$j7i0"9os\tpmue\'ANb[-^VY2rK?zU@ÿX(;…D',
+        '3${Qe7;YL]y|gz*Ju!w+§C<%=TqþS5N:lG\n/,-iEk\\p0IA ?_sW(^H9#4÷Rc@Pb~dfB¥€£xj\tn6XvU>Khr&D12"8ot}[FO\'Mma.ÿZ`…V)',
+        '3\t~c^$07DwXA?4{mn§]5BK\\\'b=þ`6H&ryYJ/v[kZ>!gG*WxI"%+,Q.\n}÷ sE)F|d#Op¥€£qTuSl9tR_8zhVfL@NCa;-oe1i<:Mjÿ(P…2U',
+        '3Ua21fz\'&+`$rR}#4B§eF<~P,\nGyiþ lv^[H*xcA;bZ{gm:uO%W>XY?)_nq÷jKV/ts-J0h¥€£@w"\\.C\tDM8EI]oTSp9!k|d5QL6ÿ(…7N=',
+        '3K\\h0iu9GSn;mV}1^[2§r)\nFzQq\'Z<þ&a5vWH|pAx:t\to{#DwX`T*"O,gR÷ e8+l?dN4U@->Y!cL7b/~(._M]J¥€£j$=BPkfIEs…ÿyC6%',
+        '3D+0k)`;zOGMeg@d=cfR]§s"N#A?P\n4i\\/þrJlq&\'EW}:-|F87\tVC!Z.2Y%÷o, KSu1{<(^hIUavBp$~6Xwyxb*H>t[9¥€…£n5mLÿjT_Q',
+        '3dpx~;Uq&)bQ*gAVjv=/]-[D\\tyR§z?8BO_\'>Gþ{$LJ0|2n}XMaF<NY5l,.^#W÷"mEkwP+4:T\t(7Cf hru…6%\niH@S¥€£K9`!cesÿ1IZo',
+        '3%@"WjT5N)]Rh2M6r.1f>#:[pYiE9HZ_wD|{A+7<B¥€£4þ§GyCblPUF*k}Q`08gcnoVt÷sO\'L,/!u-a~^\tdx&=KI$mJ…zq;Xv (\ne\\S?ÿ',
+        '3BG)D<oQOIt-Y8zd,?;:\'|Tpe}VAU!i7 .w$W"SgE¥€£b§þq%*#X@R0xyr9`s\n+h45a÷Fkc^K\t~{26…j1=Z>\\(m]N&Jn_lCLHPv/[uMfÿ',
+        '3:.\'Xh f8a>kyvxM*K;s=I?[cqZ)/@<pW`2(01CA-9o§bþ¥€£}YiNj!\t%w^DF~r6J$,U÷+VzBT…]Ren5G\n"#gOS{lQPt&m_udH\\47LE|ÿ',
+        '3^)0/z#O7\'UNK|w1JoH&I`PxC2\ncy5$@q9E%-Y?LeRm§tWþl}F!gh¥€£…Q[sX8G{(:M;6.÷4=_>A+vZÿ"SjT]u,naVfDip\tr\\<bB~dk* ',
+        '3o4yh>UtVX+?}*(!7m#Iirq5D]J@L\\"Sj2Kfkb:/<z)M=§ B;þ.¥€£`{%RZ8n[lY\nF,waC÷~vAWHd…$1ÿe|O-_Esg9Pup0\tN\'GcT&^xQ6'
     ]
 print('\033[0mRemoving all encryption temp files...')
-os.system('del /f encryption*.tmp')
+os.system('sudo rm -f /tmp/encryption*.tmp')
 print('Successful.\n')
 print('\033[1mPlease check the hash below to keep your data safe.\033[0m\n\033[96m-------------------------------------------------------------------')
 orig_hash = [
-    '9cf9207cf16905e44ceab21858ea05b3',
-    'c7f1e9b2ef8e5730ac902ce2f510b72a',
-    'e59b46d8ba7fa31b9f2cd3d48d72495b'
+    '2b2efae3c00465fa5cc39b3a856f7a43',
+    'c1cbd36aff7235a01b5c17e856dd55bd',
+    '0a1a2491caba54ebe0d40a2d78ffcc92'
 ]
 edah, edbh, eph = (
     __import__('hashlib').md5(str(conv1).encode()).hexdigest(),
@@ -169,8 +169,8 @@ elif i == 'r' or '-r' in __import__('sys').argv:
         if para2 == 'none' or para2 == 'None': para2 = '' 
     print(f'program launch command: {lang} {para1} {o} {para2}')
     fo.close()
-    os.system(f'move {o} %temp%\{o}')
-    os.system(f'start wt {lang} {para1} %temp%\{o} {para2}')
+    os.system(f'mv {o} /tmp/{o}')
+    os.system(f'{lang} {para1} /tmp/{o} {para2}')
     
 # Ctrl+K+U to remove note 
 # Ctrl+K+C to add note
